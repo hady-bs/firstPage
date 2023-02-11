0d11092e7d90b441cddacee0a57d191195e73b41
hady-bs <73134672+hady-bs@users.noreply.github.com>
2023-02-11 12:38:52 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+let spans = document.querySelectorAll(".range span");
+
+let skills = document.querySelector(".skills");
+
+let cards = document.querySelectorAll(".scores .container .card");
+let score = document.querySelector(".scores");
+cards.forEach((it) => {
+  it.children[0].innerText = "0";
+});
+
+let t = false;
+window.onscroll = () => {
+  if (window.scrollY >= skills.offsetTop - 250) {
+    spans.forEach((li) => {
+      li.style.width = li.getAttribute("data-range");
+    });
+  }
+  if (window.scrollY >= score.offsetTop - 200) {
+    if (!t) {
+      cards.forEach((li) => {
+        let goal = li.dataset.max;
+        let count = setInterval(() => {
+          li.children[0].innerText++;
+          if (li.children[0].innerText == goal) {
+            clearInterval(count);
+          }
+          t = true;
+        }, 1000 / goal);
+        t = true;
+      });
+    }
+  }
+};
